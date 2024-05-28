8811bc2607c95c1d7509a252eb7cccbb9dbb1f87
Omkar Ukirde <omking6699@gmail.com>
2024-05-28 11:17:04 +0530

    first nodejs full stack app

---
+++ index.js
@@ -0,0 +1,18 @@
+//write basic express boilerplate code
+// with express.json() middleware
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.post('/todos', (req,res)=>{
+
+})
+app.get('/todos', (req,res)=>{
+
+})
+app.put('/completed', (req,res)=>{
+
+})
+app.listen(3000)
